2006efb3f4ad0d04a766d05b6c2dfe5c8cc08184
ColinJWright <colinwright2771@gmail.com>
2021-01-28 18:44:16 +0000

    Lesson 88 complete. Getting the game to reset when a goal is scored.

--- Level/Level.gd
+++ Level/Level.gd
@@ -1,6 +1,9 @@
 extends Spatial
 
-
-
 func _on_GoalDetector_body_entered(body, goal_id):
 	print("Player" + str(goal_id) + " has scored")
+	get_tree().call_group("game_pieces", "freeze")
+	$Timer.start()
+
+func _on_Timer_timeout():
+	get_tree().call_group("game_pieces", "reset")
