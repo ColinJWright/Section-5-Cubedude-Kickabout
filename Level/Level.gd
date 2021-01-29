83d9ac893954a4842a45846f066996024df6ba6b
ColinJWright <colinwright2771@gmail.com>
2021-01-29 15:57:45 +0000

    Lesson 89 complete. Added a GUI with score tracking / display.

--- Level/Level.gd
+++ Level/Level.gd
@@ -1,9 +1,26 @@
 extends Spatial
 
+var player1_score = 0
+var player2_score = 0
+
 func _on_GoalDetector_body_entered(body, goal_id):
 	print("Player" + str(goal_id) + " has scored")
 	get_tree().call_group("game_pieces", "freeze")
 	$Timer.start()
+	update_score(goal_id)
 
 func _on_Timer_timeout():
 	get_tree().call_group("game_pieces", "reset")
+
+func update_score(player):
+	var new_score
+	
+	match player:
+		1:
+			player1_score += 1
+			new_score = player1_score
+		2:
+			player2_score += 1
+			new_score = player2_score
+			
+	$GUI.update_score(player, new_score)
